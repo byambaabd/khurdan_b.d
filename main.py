5bcc4859e31e9dc99ff0755148da9279584da81e
byambaabd <bybaj2@gmail.com>
2024-12-03 10:54:08 +0800

    12.03 byambaa

---
+++ main.py
@@ -0,0 +1,33 @@
+# a = 12 
+# b = 24
+
+# # c = a + b
+# # print (c)
+
+# # c = a ** b 
+# # print(c)
+
+# # c = a/b
+# # print(c)
+
+# # c = a % b
+# # print(c)
+
+# if a > b:
+#     print("a ni b-s ih shuu")
+# elif a == b:
+#     print("a b tentsuu shuu")
+# else:
+#     print("b ni a aas ih shuu")
+
+# for i in range(1,10,2):
+#     print(i)
+# items = ['apple', 'banana', 'pineapple']
+# for item in items:
+#     if item == 'banana':
+#         print("gadil ymaa gj")
+# s = 0 
+# for i in range(1,11):
+#     s = s + i
+# print(s)
+print(sum(range(11)))
